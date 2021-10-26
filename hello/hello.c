2d95703af0fcfa9af0b43559b31bdadbc9363951
Mingzhang Deng <wu20880@bristol.ac.uk>
2021-10-26 14:53:08 +0100

    Finish 2 questions // Second one still have space to improve

--- hello/hello.c
+++ hello/hello.c
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void *hello_world(void *args) {
     int *n = args;
@@ -7,13 +8,27 @@ void *hello_world(void *args) {
     pthread_exit(NULL);
 }
 
+typedef struct Value {
+    int value;
+}Value;
+
 int main(int argc, char const *argv[]) {
-    pthread_t thread;
-    int n = 1;
-    if (pthread_create(&thread, NULL, hello_world, &n)) {
-        printf("Error creating thread\n");
+    pthread_t *threads = (pthread_t *)malloc(sizeof(pthread_t)*5);
+    for(int n = 1;n<=5;n++){
+        int* num = malloc(sizeof(int));
+        *num = n;
+        if (pthread_create(&threads[n-1], NULL, hello_world, num)) {
+            printf("Error creating thread\n");
+        }
+
     }
-    if (pthread_join(thread, NULL)) {
-        printf("Error joining thread\n");
+    for(int n = 1;n<=5;n++){
+        if (pthread_join(threads[n-1], NULL)) {
+            printf("Error joining thread\n");
+        }
+
     }
+
+
+
 }
--- quiz/quiz.c
+++ quiz/quiz.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 struct question {
     char *question;
@@ -10,20 +11,45 @@ struct question {
 };
 typedef struct question question_t;
 
+struct timer_args{
+    pthread_cond_t *cond;
+    pthread_mutex_t  *timerMutex;
+    int *timeup;
+};
+typedef struct timer_args timer_args_t;
+
 struct ask_args {
-    int score;
+    int *score;
     question_t *question;
+    pthread_mutex_t *mutex;
+    pthread_cond_t  *worker;
+    int *timeup;
+    int q_number;
+
 };
 typedef struct ask_args ask_args_t;
 
 void *timer(void *args) {
+    timer_args_t *timerArgs = args;
+    int *timeup = timerArgs->timeup;
+    pthread_mutex_lock(timerArgs->timerMutex);
     sleep(5);
+    printf("\n");
     printf("5s have passed\n");
+    *timeup = 1;
+    pthread_mutex_unlock(timerArgs->timerMutex);
+    pthread_cond_broadcast(timerArgs->cond);
     pthread_exit(NULL);
 }
 
 void *ask(void *args) {
     ask_args_t *ask_args = args;
+    pthread_mutex_lock(ask_args->mutex);
+    while(*(ask_args->timeup) == 1){
+        pthread_cond_wait(ask_args->worker,ask_args->mutex);
+    }
+
+    int *score = ask_args->score;
 
     printf("%s? ", ask_args->question->question);
 
@@ -35,11 +61,16 @@ void *ask(void *args) {
 
     if (!strcmp(input, ask_args->question->answer)) {
         printf("Correct!\n");
-        ask_args->score++;
+        *score = *score+1;
     } else {
         printf("Incorrect :-(\n");
     }
 
+    printf("You answered question %d\n",ask_args->q_number);
+    ///
+    pthread_cond_broadcast(ask_args->worker);
+    pthread_mutex_unlock(ask_args->mutex);
+    ///
     pthread_exit(NULL);
 }
 
@@ -52,25 +83,39 @@ int main(int argc, char const *argv[]) {
         {.question = "3+3", .answer = "6"},
         {.question = "4/2", .answer = "2"}};
 
+    ///
+
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t  timerMutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_cond_t timeup = PTHREAD_COND_INITIALIZER;
+    pthread_cond_t worker = PTHREAD_COND_INITIALIZER;
+    int timeupFlag = 0;
+
+
+    ///
+
     pthread_t timer_thread;
-    if (pthread_create(&timer_thread, NULL, timer, NULL)) {
+    timer_args_t timerArgs = {.cond=&timeup,.timeup = &timeupFlag,.timerMutex = &timerMutex};
+    if (pthread_create(&timer_thread, NULL, timer, &timerArgs)) {
         printf("Error creating timer thread.\n");
     }
 
     int score = 0;
     for (int i = 0; i < 6; ++i) {
-        ask_args_t ask_args = {.score = score, .question = &questions[i]};
-
+        ask_args_t *ask_args = malloc(sizeof(ask_args_t));
+        ask_args_t ask_arg = {.score = &score, .question = &questions[i],.mutex=&mutex, .timeup=&timeupFlag,.worker = &worker,.q_number=i};
+        *ask_args = ask_arg;
         pthread_t ask_thread;
-        if (pthread_create(&ask_thread, NULL, ask, &ask_args)) {
+        if (pthread_create(&ask_thread, NULL, ask, ask_args)) {
             printf("Error creating asker thread.\n");
         }
-
-        if (pthread_join(ask_thread, NULL)) {
-            printf("Error joining asker thread.\n");
-        }
-        score = ask_args.score;
     }
+
+    while (timeupFlag != 1){
+        pthread_cond_wait(&timeup,&timerMutex);
+    }
+
+    //pthread_mutex_lock(&mutex);
 
     printf("End of questions, final score %d\n", score);
     return 0;
